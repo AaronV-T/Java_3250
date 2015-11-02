039f24840a67567093f139959c180bd32b5ce672
twhiting <trentonwhiting@msn.com>
2015-11-02 16:16:27 -0700

    New Project
    I removed all the code that was not needed for this assignment  and
    cleaned the code up so you guys understand whats going on better.

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Java Final Assignment - Twitter Streaming API
+
+# Setup
+
+We need to create a twitter account (Chad)
+Here: https://apps.twitter.com/
+
+You will then need to edit the "twitter4j_properties" file with the associated key & secrets. This
+file must be in the project root directory.
+For configuration see #1 here: http://twitter4j.org/en/configuration.html 
+
---
+++ src/Twitter.java
@@ -0,0 +1,125 @@
+import twitter4j.*;
+import java.util.*;
+
+public class Twitter {
+
+    /* Main */
+    public static void main(String[] args) {
+        Streaming streamer = new Streaming();
+
+        try {
+            //Try to open stream using the streamer class
+            streamer.streaming();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /* Handles all connections to the Twitter Streaming API */
+    static class Streaming {
+
+        public void streaming() {
+
+            Scanner reader = new Scanner(System.in);
+            String given         = "";                                  //For search term
+            int number_of_tweets = 0;                                   //Total number of tweets, default at 25
+
+            try
+            {
+                /* Ask for search term */
+                while(given.equals("")) {
+                    System.out.println("search term?");
+                    System.out.print("> "); given = reader.nextLine();
+
+                    System.out.println();
+                }
+
+                given = given.toLowerCase();
+                given = given.trim();
+
+                /*Get number of Tweets*/
+                while(number_of_tweets == 0) {
+                    System.out.println("# of tweets to catch?");
+                    System.out.print("> "); number_of_tweets = reader.nextInt();
+                    System.out.println();
+                }
+
+                final int final_tweets = number_of_tweets;              //Inner class needs a final variable
+
+                /* Open stream */
+                TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+
+                //Implement a listener
+                StatusListener listener = new StatusListener() {
+                    public int counter = 1;
+
+                    @Override
+                    public void onStatus(Status status) {
+                        boolean stream_status = stream_watcher(final_tweets);
+
+                        /*Print tweet to stream if we are under the cap */
+                        if (stream_status) {
+                            twitterStream.clearListeners();       //reached cap, shutdown stream
+                            twitterStream.shutdown();
+                        }
+                        else {
+                            print_tweet(counter++, status);
+                        }
+                    }
+
+                    @Override
+                    public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                        //System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+                    }
+
+                    @Override
+                    public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                        System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+                    }
+
+                    @Override
+                    public void onScrubGeo(long userId, long upToStatusId) {
+                        System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+                    }
+
+                    @Override
+                    public void onStallWarning(StallWarning warning) {
+                        System.out.println("Got stall warning:" + warning);
+                    }
+
+                    @Override
+                    public void onException(Exception ex) {
+                        ex.printStackTrace();
+                    }
+                };
+
+                /* add our listener to the stream */
+                twitterStream.addListener(listener);
+
+                /* Create filter for english & Search Term */
+                FilterQuery stream_Filter = new FilterQuery(given);
+                stream_Filter.language("en");                           //English only
+                twitterStream.filter(stream_Filter);                    //apply filter to stream
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+        /* Used to print a user and their tweet */
+        private static void print_tweet(int index, Status s) {
+            String tweet = s.getText().toLowerCase();
+            if (index > 0)
+                System.out.println("\n[" + index + "]");
+            System.out.println(s.getUser().getName() + " @" + s.getUser().getScreenName() + "Tweet: " + tweet);
+        }
+
+        /* Needed to check if stream is below threshold */
+        static int total_tweets = 0;
+        static boolean stream_watcher( int max_tweets ) {
+            return ++total_tweets > max_tweets;
+        }
+
+    }
+}
