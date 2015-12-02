517943a32d7659641c5d178ed6d40000e8fcf884
AaronV-T <avtholl@hotmail.com>
2015-12-02 08:16:41 -0700

    CleanTweet method added and added functionality to send and receive from the Sentence Clustering API

--- src/Twitter.java
+++ src/Twitter.java
@@ -1,15 +1,19 @@
 import twitter4j.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
 import java.util.*;
+import java.net.URL;
 
 public class Twitter {
 
     /* Main */
     public static void main(String[] args) {
         Streaming streamer = new Streaming();
-
+        LinkedHashSet<String> storeTweets = new LinkedHashSet<>();
         try {
             //Try to open stream using the streamer class
-            streamer.streaming();
+            streamer.streaming(storeTweets);
         }
         catch (Exception e) {
             System.out.println(e);
@@ -19,7 +23,7 @@ public class Twitter {
     /* Handles all connections to the Twitter Streaming API */
     static class Streaming {
 
-        public void streaming() {
+        public void streaming(LinkedHashSet<String> storeTweets) {
 
             Scanner reader = new Scanner(System.in);
             String given         = "";                                  //For search term
@@ -61,10 +65,17 @@ public class Twitter {
                         /*Print tweet to stream if we are under the cap */
                         if (stream_status) {
                             twitterStream.clearListeners();       //reached cap, shutdown stream
+                            toJson(storeTweets);
                             twitterStream.shutdown();
                         }
                         else {
-                            print_tweet(counter++, status);
+                            print_tweet(counter++, status, storeTweets);
+                            String tweet = status.getText();
+                            if (status.isRetweet()) //If this is a re-tweet, get the original tweet (To avoid possible loss of characters on the end)
+                                tweet = status.getRetweetedStatus().getText();
+
+                            tweet = CleanTweet(tweet);
+                            storeTweets.add(tweet);
                         }
                     }
 
@@ -107,12 +118,86 @@ public class Twitter {
             }
         }
 
+        private static String CleanTweet(String rawTweet) {
+            rawTweet = rawTweet.toLowerCase();
+            String[] words = rawTweet.split("\\s+"); //Split the tweet into an array of strings.
+            String cleanedTweet = "";
+
+            for (int i = 0; i < words.length; i++) {
+                boolean includeThisWord = true;
+                if (i == 0 && words[i].equals("rt")) //Don't include word that specifies re-tweets.
+                    includeThisWord = false;
+                else if (words[i].indexOf("@") == 0) //Don't include any user names.
+                    includeThisWord = false;
+                else if (words[i].indexOf("http://") == 0 || words[i].indexOf("https://") == 0) //Don't include links.
+                    includeThisWord = false;
+
+
+                words[i] = words[i].replaceAll("[^a-zA-Z0-9]", ""); //Remove all non alphanumeric characters.
+
+                if (words[i].length() == 0)
+                    includeThisWord = false;
+
+                if (includeThisWord) {
+                    cleanedTweet += words[i] + " "; //Add this word to the cleaned tweet.
+                }
+            }
+
+            if (cleanedTweet.length() > 0)
+                cleanedTweet = cleanedTweet.substring(0, cleanedTweet.length() - 1); //Remove last character (which is a space).
+            return cleanedTweet;
+        }
+
         /* Used to print a user and their tweet */
-        private static void print_tweet(int index, Status s) {
-            String tweet = s.getText().toLowerCase();
-            if (index > 0)
-                System.out.println("\n[" + index + "]");
-            System.out.println(s.getUser().getName() + " @" + s.getUser().getScreenName() + "Tweet: " + tweet);
+        private static void print_tweet(int index, Status s, LinkedHashSet theSet) {
+            System.out.println("\n[" + index + "]");
+            System.out.println("User: " + s.getUser().getScreenName() + ", Tweet: " + s.getText());
+        }
+
+        private static void toJson(LinkedHashSet<String> theSet){
+            try{
+                StringBuffer jsonBuffer = new StringBuffer();
+                jsonBuffer.append("{\"type\":\"pre-sentenced\",\"text\":[");
+
+                for(String twt:theSet){ //For every string in the set of tweet strings, append it to our jsonBuffer.
+                    jsonBuffer.append("{\"sentence\":\""+twt+"\"},");
+                }
+                if (jsonBuffer.charAt(jsonBuffer.length() - 1) == ',') //If the last character is a comma, remove it.
+                    jsonBuffer.deleteCharAt(jsonBuffer.length() - 1);
+                jsonBuffer.append("]}");
+
+                System.out.println(jsonBuffer.toString());
+
+                URL postURL = new URL("https://rxnlp-core.p.mashape.com/generateClusters");
+                HttpsURLConnection conn = (HttpsURLConnection) postURL.openConnection();
+
+                //Set request headers.
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("X-Mashape-Key", "6KlwDbtPVHmshIgWuSzH7z574UOzp1k4TLyjsnFORCaklTMv9k");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+
+                conn.setDoOutput(true);
+                try (DataOutputStream  writer = new DataOutputStream(conn.getOutputStream());) {
+                    writer.writeBytes(jsonBuffer.toString());
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace(System.out);
+                }
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));) { //This is currently receiving a 402 response from the URL.
+                    String line;
+                    System.out.println("Reading response.");
+                    while ((line = reader.readLine()) != null)
+                        System.out.println(line);
+                } catch (IOException e) {
+                    e.printStackTrace(System.out);
+                    System.out.println(conn.getHeaderField("Connection"));
+                    System.out.println(conn.getHeaderField("Content-Length"));
+                }
+
+            }catch(Exception e){
+                e.printStackTrace(System.out);
+            }
         }
 
         /* Needed to check if stream is below threshold */
