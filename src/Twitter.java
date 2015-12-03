3fe2a5be6a5bcf583d2b2c05c1a168627fca00ea
cwardell77 <corey.wardell123@gmail.com>
2015-12-02 17:14:58 -0700

    Changed the API key and it worked

--- src/Twitter.java
+++ src/Twitter.java
@@ -1,16 +1,19 @@
 import twitter4j.*;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
 import java.util.*;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.HttpResponse;
+import java.net.URL;
+
 
 public class Twitter {
 
     /* Main */
     public static void main(String[] args) {
         Streaming streamer = new Streaming();
-        TreeSet<String> storeTweets = new TreeSet<>();
-        try {
+        LinkedHashSet<String> storeTweets = new LinkedHashSet<>();
+
+        try
+        {
             //Try to open stream using the streamer class
             streamer.streaming(storeTweets);
         }
@@ -21,8 +24,7 @@ public class Twitter {
 
     /* Handles all connections to the Twitter Streaming API */
     static class Streaming {
-
-        public void streaming(TreeSet<String> storeTweets) {
+        public void streaming(LinkedHashSet<String> storeTweets) {
 
             Scanner reader = new Scanner(System.in);
             String given         = "";                                  //For search term
@@ -68,7 +70,16 @@ public class Twitter {
                             twitterStream.shutdown();
                         }
                         else {
-                            print_tweet(counter++, status,storeTweets);
+
+                            String tweet = status.getText();
+
+                            //If this is a re-tweet, get the original tweet (To avoid possible loss of characters on the end)
+                            if (status.isRetweet())
+                                tweet = status.getRetweetedStatus().getText();
+
+                            tweet = CleanTweet(tweet);
+                            storeTweets.add(tweet);
+                            print_tweet(counter++, status); //Verbose for user
                         }
                     }
 
@@ -106,35 +117,110 @@ public class Twitter {
                 stream_Filter.language("en");                           //English only
                 twitterStream.filter(stream_Filter);                    //apply filter to stream
 
-
-
             } catch (Exception e) {
                 System.out.println(e);
             }
         }
 
-        /* Used to print a user and their tweet */
-        private static void print_tweet(int index, Status s,TreeSet theSet) {
-            String tweet = s.getText().toLowerCase();
-            if (index > 0)
-            //    System.out.println("\n[" + index + "]");
-            //System.out.println(s.getUser().getName() + " @" + s.getUser().getScreenName() + "Tweet: " + tweet);
-            theSet.add(s.getUser().getName() + "@" + s.getUser().getScreenName() + "Tweet" + tweet);
+        private static String CleanTweet(String rawTweet) {
+            rawTweet = rawTweet.toLowerCase();
+            String[] words = rawTweet.split("\\s+"); //Split the tweet into an array of strings.
+            String cleanedTweet = "";
+
+            for (int i = 0; i < words.length; i++) {
+                boolean includeThisWord = true;
+                if (i == 0 && words[i].equals("rt")) //Don't include word that specifies re-tweets.
+                    includeThisWord = false;
+                else if (words[i].indexOf("@") == 0) //Don't include any user names.
+                    includeThisWord = false;
+                else if (words[i].indexOf("http://") == 0 || words[i].indexOf("https://") == 0) //Don't include links.
+                    includeThisWord = false;
+
+
+                words[i] = words[i].replaceAll("[^a-zA-Z0-9]", ""); //Remove all non alphanumeric characters.
+
+                if (words[i].length() == 0)
+                    includeThisWord = false;
+
+                if (includeThisWord) {
+                    cleanedTweet += words[i] + " "; //Add this word to the cleaned tweet.
+                }
+            }
+
+            if (cleanedTweet.length() > 0)
+                cleanedTweet = cleanedTweet.substring(0, cleanedTweet.length() - 1); //Remove last character (which is a space).
+            return cleanedTweet;
         }
 
-        private static void toJson(TreeSet<String> theSet){
-            try{
-                String jsonQuery = "{\"type\": \"pre-sentenced\",\"text\": [";
-                Iterator<String> it = theSet.iterator();
-                for(String twt:theSet){
-                    jsonQuery+="{\"sentence\": \""+twt+"\"},";
+        /* Used to print a user and their tweet */
+        private static void print_tweet(int index, Status s) {
+            System.out.println("\n[" + index + "]");
+            System.out.println("User: " + s.getUser().getScreenName() + ", Tweet: " + s.getText());
+        }
+
+        private static void toJson(LinkedHashSet<String> theSet) {
+            try {
+                StringBuffer jsonBuffer = new StringBuffer();
+                jsonBuffer.append("{\"type\":\"pre-sentenced\",\"text\":[");
+
+                for (String twt : theSet) { //For every string in the set of tweet strings, append it to our jsonBuffer.
+                    jsonBuffer.append("{\"sentence\":\"" + twt + "\"},");
                 }
-                jsonQuery+="]}";
-                System.out.println(jsonQuery);
-            }catch(Exception e){
-                e.printStackTrace(System.out);
+
+                //If the last character is a comma, remove it.
+                if (jsonBuffer.charAt(jsonBuffer.length() - 1) == ',') {
+                    jsonBuffer.deleteCharAt(jsonBuffer.length() - 1);
+                }
+
+                jsonBuffer.append("]}");
+
+                System.out.println(jsonBuffer.toString());
+
+                URL postURL = new URL("https://rxnlp-core.p.mashape.com/generateClusters");
+                HttpsURLConnection conn = (HttpsURLConnection) postURL.openConnection();
+
+                //Set request headers.
+                conn.setRequestMethod("POST");
+               // conn.setRequestProperty("X-Mashape-Key", "6KlwDbtPVHmshIgWuSzH7z574UOzp1k4TLyjsnFORCaklTMv9k");
+                conn.setRequestProperty("X-Mashape-Key", "3qhKsAzciTmsh2xJmNjMWclaHDuDp1vcvYBjsnpMkIIbebgLRx");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoOutput(true);
+
+                try (DataOutputStream writer = new DataOutputStream(conn.getOutputStream())) {
+                    writer.writeBytes(jsonBuffer.toString());
+                    writer.flush();
+                }
+                catch (IOException e) {
+                    e.printStackTrace(System.out);
+                }
+
+                //This is currently receiving a 402 response from the URL.
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                    String line;
+                    System.out.println("Reading response.");
+
+                    while ((line = reader.readLine()) != null)
+                        System.out.println(line);
+
+                } catch (IOException e) {
+                    e.printStackTrace(System.out);
+                    System.out.println(conn.getHeaderField("Connection"));
+                    System.out.println(conn.getHeaderField("Content-Length"));
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
             }
         }
+
+        /*
+        private static void print_tweet(int index, Status s, LinkedHashSet theSet) {
+            String tweet = s.getText().toLowerCase();
+            if (index > 0)
+            {
+                theSet.add(s.getUser().getName() + "@" + s.getUser().getScreenName() + " Tweet" + tweet);
+            }
+        } */
 
         /* Needed to check if stream is below threshold */
         static int total_tweets = 0;
