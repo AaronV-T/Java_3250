5fc2f7c0ce94e48f50f1bee392d39c2fd0b0b28f
Chad D <chaderick9@hotmail.com>
2015-12-05 10:40:41 -0700

    Remade the project and cleaned all code that was unnecessary from my old project.

--- src/Twitter.java
+++ src/Twitter.java
@@ -8,70 +8,80 @@ import org.json.JSONObject;
 
 public class Twitter {
     public static void main(String[] args) {
+        /* local vars */
         String[] searchTerms;
         int[] numberOfTweets;
-        int number_of_search_terms = 0, search_term = 1;
+        int numberOfSearchTerms, searchTerm = 1, minNumOfTweets = 10; /* change minNumOfTweets HERE!!! */
 
+        /* terminal -- no GUI */
         if(args.length == 0){
-            // local vars
             Scanner reader = new Scanner(System.in);
-        /* Ask for number of search terms */
+            /* ask for number of search terms */
             do {
                 System.out.println("1 or 2 search terms?");
                 System.out.print("> ");
                 if (reader.hasNextInt()) {
-                    number_of_search_terms = reader.nextInt();
-                    System.out.println();
+                    numberOfSearchTerms = reader.nextInt();
+                    if (numberOfSearchTerms > 0 && numberOfSearchTerms <= 2) {
+                        reader.nextLine();
+                        System.out.println();
+                        break;
+                    }
                 } else {
-                    System.out.println("Error -- Usage: [integer] greater than 0\n");
+                    reader.next(); /* account for string as input */
                 }
-
-                reader.nextLine();
-            } while (number_of_search_terms < 1 || number_of_search_terms > 2);
-
-            searchTerms = new String[number_of_search_terms];
-            numberOfTweets = new int[number_of_search_terms];
-        /* master loop, collecting tweets for each search term */
-            for (int i = 0; i < number_of_search_terms; i++) {
-        /* Ask for search term */
+                System.out.println("Error -- Usage: [integer: 1 | 2]\n");
+            } while (true);
+            
+            searchTerms = new String[numberOfSearchTerms];
+            numberOfTweets = new int[numberOfSearchTerms];
+            
+            /* master loop, collecting tweets for each search term */
+            for (int i = 0; i < numberOfSearchTerms; i++) {
+            /* ask for search term */
                 do {
-                    System.out.println("search term " + search_term++ + "?");
+                    System.out.println("search term " + searchTerm + "?");
                     System.out.print("> ");
                     searchTerms[i] = reader.nextLine().toLowerCase().trim();
-                    System.out.println();
-                    if (!searchTerms[i].equals("")) break;
-                    else System.out.println("Error -- Usage: [String] anything really... come on.\n");
+                    if (!searchTerms[i].equals("")) {
+                        System.out.println();
+                        searchTerm++;
+                        break;
+                    }
+                    System.out.println("Error -- Usage: [String: anything really... come on]\n");
                 } while (searchTerms[i].equals(""));
 
-            /* Get number of Tweets */
+            /* get number of Tweets */
                 do {
-                    System.out.println("# of tweets to catch? ( min 500 )");
+                    System.out.println("# of tweets to catch? ( min " + minNumOfTweets + " )");
                     System.out.print("> ");
                     if (reader.hasNextInt()) {
                         numberOfTweets[i] = reader.nextInt();
-                        System.out.println();
+                        if (numberOfTweets[i] >= minNumOfTweets) {
+                            reader.nextLine();
+                            System.out.println();
+                            break;
+                        }
                     } else {
-                        System.out.println("Error -- Usage: [integer] greater than 10\n");
+                        reader.next(); /* account for string as input */
                     }
-
-                    reader.nextLine();
-                } while (numberOfTweets[i] < 10);
+                    System.out.println("Error -- Usage: [integer: >= " + minNumOfTweets + "]\n");
+                } while (true);
             }
         }
-        else
-        {
-            //args [String 1] [tweet count 1] [String 2] [Tweet count 2]
+
+        /* GUI -- args provided */
+        else {
+            /* args [String 1] [tweet count 1] [String 2] [Tweet count 2] */
             if(args.length == 2){
-                //One search
-                number_of_search_terms = 1;
+                /* One search */
+                numberOfSearchTerms = 1;
                 numberOfTweets = new int[1];
                 numberOfTweets[0] = Integer.parseInt(args[1]);
                 searchTerms = new String[1];
                 searchTerms[0] = args[0];
-            }
-            else
-            {
-                number_of_search_terms = 2;
+            } else {
+                numberOfSearchTerms = 2;
                 numberOfTweets = new int[2];
                 numberOfTweets[0] = Integer.parseInt(args[1]);
                 numberOfTweets[1] = Integer.parseInt(args[3]);
@@ -81,21 +91,21 @@ public class Twitter {
             }
         }
 
-        for (int i = 0; i < number_of_search_terms; i++) { //For each search term, create a new Streaming object and run it.
+        /* for each search term, create a new Streaming object and run it */
+        for (int i = 0; i < numberOfSearchTerms; i++) {
             System.out.println("Starting search for: " + searchTerms[i]);
             Streaming streaming = new Streaming(searchTerms[i], numberOfTweets[i]);
             streaming.Start();
         }
     }
 
-    /* Handles all connections to the Twitter Streaming API */
+    /* handles all connections to the Twitter Streaming API */
     static class Streaming {
-        // local vars
+        /* local vars */
         LinkedHashSet<String> storeTweets = new LinkedHashSet<>();
         LinkedHashSet<String> originalTweets = new LinkedHashSet<>();
+        int number_of_tweets, total_tweets = 0;
         String given;
-        int number_of_tweets;
-        int total_tweets = 0;
 
         /* constructor */
         Streaming(String term, int numTweets) {
@@ -103,15 +113,16 @@ public class Twitter {
             number_of_tweets = numTweets;
         }
 
+        /* method used to open twitter4j api, and collect tweets */
         public void Start() {
             try
             {
                 final int final_tweets = this.number_of_tweets;
 
-                /* Open stream */
+                /* open stream */
                 TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 
-                //Implement a listener
+                /* implement a listener */
                 StatusListener listener = new StatusListener() {
                     int counter = 1;
                     boolean finished = false;
@@ -120,11 +131,11 @@ public class Twitter {
                     public void onStatus(Status status) {
                         boolean stream_status = stream_watcher(final_tweets);
 
-                        /*Print tweet to stream if we are under the cap */
-                        if (stream_status) { //If we have reached the tweet limit...
-                            if (!finished) { //If this hasn't already happened.
+                        /* print tweet to stream if we are under the cap */
+                        if (stream_status) { /* if we have reached the tweet limit... */
+                            if (!finished) { /* if this hasn't already happened */
                                 finished = true;
-                                twitterStream.clearListeners(); //Remove the listeners from our twitterStream object.
+                                twitterStream.clearListeners(); /* remove the listeners from our twitterStream object */
                                 ClusterAndSave(storeTweets, given);
                                 twitterStream.cleanUp();
                             }
@@ -133,13 +144,13 @@ public class Twitter {
                             String tweet = status.getText();
                             originalTweets.add(tweet);
 
-                            if (status.isRetweet()) //If this is a re-tweet, get the original tweet (To avoid possible loss of characters on the end)
+                            if (status.isRetweet()) /* if this is a re-tweet, get the original tweet (To avoid possible loss of characters on the end) */
                                 tweet = status.getRetweetedStatus().getText();
 
                             tweet = CleanTweet(tweet);
                             storeTweets.add(tweet);
-                            System.out.println(given + ": " + counter);
-                            // print_tweet(counter, status); //Verbose for user
+
+                            System.out.println(given + ": " + counter); /* print_tweet(counter, status); verbose for user */
                             counter++;
                         }
                     }
@@ -173,46 +184,49 @@ public class Twitter {
                 /* add our listener to the stream */
                 twitterStream.addListener(listener);
 
-                /* Create filter for english & Search Term */
+                /* create filter for english & Search Term */
                 FilterQuery stream_Filter = new FilterQuery(given);
-                stream_Filter.language("en");                           //English only
-                twitterStream.filter(stream_Filter);                    //apply filter to stream
+                stream_Filter.language("en");                           /* english only */
+                twitterStream.filter(stream_Filter);                    /* apply filter to stream */
 
             } catch (Exception e) {
-                System.out.println(e);
+                System.out.println(e.getMessage());
             }
         }
 
+        /* method used to clean up the tweet as it streams in from the API
+           returns: String cleanedTweet */
         private String CleanTweet(String rawTweet) {
             rawTweet = rawTweet.toLowerCase();
-            String[] words = rawTweet.split("\\s+"); //Split the tweet into an array of strings.
+            String[] words = rawTweet.split("\\s+"); /* split the tweet into an array of strings */
             String cleanedTweet = "";
 
             for (int i = 0; i < words.length; i++) {
                 boolean includeThisWord = true;
-                if (i == 0 && words[i].equals("rt")) //Don't include word that specifies re-tweets.
+                if (i == 0 && words[i].equals("rt")) /* don't include word that specifies re-tweets */
                     includeThisWord = false;
-                else if (words[i].indexOf("@") == 0) //Don't include any user names.
+                else if (words[i].indexOf("@") == 0) /* don't include any user names */
                     includeThisWord = false;
-                else if (words[i].indexOf("http://") == 0 || words[i].indexOf("https://") == 0) //Don't include links.
+                else if (words[i].indexOf("http://") == 0 || words[i].indexOf("https://") == 0) /* Don't include links */
                     includeThisWord = false;
 
-                words[i] = words[i].replaceAll("[^a-zA-Z0-9]", ""); //Remove all non alphanumeric characters.
+                words[i] = words[i].replaceAll("[^a-zA-Z0-9]", ""); /* remove all non alphanumeric characters */
 
                 if (words[i].length() == 0)
                     includeThisWord = false;
 
                 if (includeThisWord) {
-                    cleanedTweet += words[i] + " "; //Add this word to the cleaned tweet.
+                    cleanedTweet += words[i] + " "; /* add this word to the cleaned tweet */
                 }
             }
 
             if (cleanedTweet.length() > 0)
-                cleanedTweet = cleanedTweet.substring(0, cleanedTweet.length() - 1); //Remove last character (which is a space).
+                cleanedTweet = cleanedTweet.substring(0, cleanedTweet.length() - 1); /* remove last character (which is a space) */
 
             return cleanedTweet;
         }
 
+        /* ****** Aaron's method he is still making changes to ****** */
         private void ClusterAndSave(LinkedHashSet<String> tweetSet, String searchTerm) {
             StringBuffer jsonBuffer = new StringBuffer();
             jsonBuffer.append("{\"type\":\"pre-sentenced\",\"text\":[");
@@ -248,7 +262,7 @@ public class Twitter {
 
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
                     String line;
-                    System.out.println("For search term: " + searchTerm + ". Unique Sentences: " + tweetSet.size());
+                    System.out.println("\nFor search term: " + searchTerm + ". Unique Sentences: " + tweetSet.size());
                     System.out.println(jsonBuffer.toString());
 
                     while ((line = reader.readLine()) != null)
@@ -279,9 +293,9 @@ public class Twitter {
             }
         }
 
-        /* Needed to check if stream is below threshold */
+        /* needed to check if stream is below threshold */
         boolean stream_watcher( int max_tweets ) {
-            return ++total_tweets > max_tweets; //Return true if we have reached max tweets amount
+            return ++total_tweets > max_tweets; /* return true if we have reached max tweets amount */
         }
     }
 }
